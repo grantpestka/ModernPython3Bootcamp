99779818eb01c19f2bb5e8d5dc143d68a236b042
Grant Pestka <grant.pestka@gmail.com>
2019-10-29 10:41:13 -0600

    emoji art

---
+++ Section_10/emoji_art_89.py
@@ -0,0 +1,13 @@
+# for s in range(10):
+#     c = 0
+#     while c < s:
+#         art = "\U0001f600"
+#         art = art * c
+#         print(art)
+#         c += 1
+
+for num in range(1,11):
+    art = "\U0001f600"
+    art = art * num
+    print(art)
+    
