c4bec2537f90892dce8f061923cee8770c4af36e
Grant Pestka <grant.pestka@gmail.com>
2019-10-29 10:45:51 -0600

    emoji art reftactor

--- Section_10/emoji_art_89.py
+++ Section_10/emoji_art_89.py
@@ -1,13 +1,13 @@
-# for s in range(10):
-#     c = 0
-#     while c < s:
-#         art = "\U0001f600"
-#         art = art * c
-#         print(art)
-#         c += 1
+# for num in range(1,11):
+#     art = "\U0001f600"
+#     art = art * num
+#     print(art)
 
-for num in range(1,11):
+max = 21    
+for num in range(1,max,2):
     art = "\U0001f600"
     art = art * num
-    print(art)
-    
+    space = int((max - num)/2)
+    space = " " * space
+    print(f'{space}{art}{space}')
+  
